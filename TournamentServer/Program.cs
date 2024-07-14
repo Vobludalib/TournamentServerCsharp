996e4409f112c24c2ff02115925b9617e572da41
librickysimon <librickysimon@gmail.com>
2024-07-14 16:09:35 +0200

    Initial project creation

---
+++ TournamentServer/Program.cs
@@ -0,0 +1,15 @@
+namespace TournamentSystem;
+
+class TournamentServer()
+{
+    public static void Main(String[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        var app = builder.Build();
+
+        app.MapGet("/", () => "");
+
+        app.Run();
+    }
+}
